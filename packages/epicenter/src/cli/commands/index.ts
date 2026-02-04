81ea6885605b49bbec1e3ad527614c2358391cd6
Braden Wong <git@bradenwong.com>
2026-02-03 18:00:05 -0800

    refactor(cli): remove dead barrel file and unused exports
    - Delete cli/commands/index.ts (never imported, all imports go direct to source files)
    - Remove buildKvCommands, buildMetaCommands, buildTableCommands exports (only used internally)
    - Remove hasConfig export (only used within discovery.ts)

--- packages/epicenter/src/cli/commands/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './kv-commands.js';
-export * from './meta-commands.js';
-export * from './table-commands.js';
--- packages/epicenter/src/cli/index.ts
+++ packages/epicenter/src/cli/index.ts
@@ -1,10 +1,6 @@
 export { createCLI } from './cli';
-export { buildKvCommands } from './commands/kv-commands';
-export { buildMetaCommands } from './commands/meta-commands';
-export { buildTableCommands } from './commands/table-commands';
 export {
 	type AnyWorkspaceClient,
-	hasConfig,
 	resolveWorkspace,
 	type WorkspaceResolution,
 } from './discovery';
